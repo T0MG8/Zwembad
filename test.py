3c66b818bdff24606c6fbfe99ff7ca2ce77050d9
T0MG8 <tom.goede@hva.nl>
2025-07-12 16:41:27 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 # Cache functie om data op te halen, zonder dat Streamlit de 'conn' hoeft te hashen
 @st.cache_data(ttl=30)
 def get_data(_conn, worksheet):
-    return _conn.read(worksheet=worksheet).dropna(how="all")
+    return _conn.fillna("")
 
 # Inlogstatus bijhouden
 if 'ingelogd' not in st.session_state:
